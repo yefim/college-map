2d706985656933769dcc0f1f3b5e5d790765a63e
Yefim Vedernikoff <yefim323@gmail.com>
2018-03-29 16:41:42 -0700

    Load gmaps

--- src/index.js
+++ src/index.js
@@ -2,8 +2,24 @@ import req from './req';
 
 console.log('hello world');
 
-req({url: '/v1/users/me?fields=earnings', method: 'get'}).then(function(user) {
-  user.earnings.forEach(function(earning) {
-    console.log(earning.collegeId);
+req({url: '/v1/users/me?fields=followedCollegeIds', method: 'get'}).then(function({followedCollegeIds}) {
+  followedCollegeIds.forEach(function(collegeId) {
+    req({url: `/v1/colleges/${collegeId}?fields=amount,loc,name,logo`, method: 'get'}).then(function(college) {
+      console.log(college);
+    });
   });
 });
+
+window.initMap = function() {
+  var uluru = {lat: -25.363, lng: 131.044};
+
+  var map = new google.maps.Map(document.getElementById('college-map'), {
+    zoom: 4,
+    center: uluru
+  });
+
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+};
