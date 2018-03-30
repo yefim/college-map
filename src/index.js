d35e1e94718a1183e5fb201c7227eb6aac253612
Yefim Vedernikoff <yefim323@gmail.com>
2018-03-29 17:02:20 -0700

    Load all the things

--- src/index.js
+++ src/index.js
@@ -1,35 +1,49 @@
 import req from './req';
 
 window.initMap = function() {
-  var center = {
+  const center = {
     lat: 39.5,
     lng: -98.35
   };
 
-  var map = new google.maps.Map(document.getElementById('college-map'), {
+  const map = new google.maps.Map(document.getElementById('college-map'), {
     zoom: 4,
     center: center
   });
 
-  var markers = [];
-
-  markers.push(new google.maps.Marker({
-    position: center,
-    map: map
-  }));
+  const infoWindows = [];
 
   req({url: '/v1/users/me?fields=followedCollegeIds', method: 'get'}).then(function({followedCollegeIds}) {
     followedCollegeIds.forEach(function(collegeId) {
       req({url: `/v1/colleges/${collegeId}?fields=amount,loc,name,logo`, method: 'get'}).then(function(college) {
-        var college = {
-          lat: college.loc[1],
-          lng: college.loc[0]
-        };
+        const contentString = [
+          '<div style="display: flex; flex-direction: column; align-items: center;">',
+          `<p style="font-size: 20px; font-weight: 400;">${college.name}</p>`,
+          `<img height="60" width=60" src="${college.logo}" />`,
+          college.amount ? `<p style="font-size: 16px;">You're earning $${college.amount}.</p>` : '',
+          '</div>'
+        ].join('');
 
-        markers.push(new google.maps.Marker({
-          position: college,
+        console.log(contentString);
+
+        const infoWindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+
+        infoWindows.push(infoWindow);
+
+        const marker = new google.maps.Marker({
+          position: {lat: college.loc[1], lng: college.loc[0]},
           map: map
-        }));
+        });
+
+        marker.addListener('click', function() {
+          infoWindows.forEach(function(i) {
+            i.close();
+          });
+
+          infoWindow.open(map, marker);
+        });
       });
     });
   });
